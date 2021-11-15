6ac2102704414a5adc827e725caa096a04bf10a4
snail-brain <snail-brain@protonmail.com>
2021-11-15 16:19:43 -0600

    fixed token supply

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,7 +7,7 @@ from web3 import Web3
 def deploy_token():
     account = getAccount()
     _snail = SNLToken.deploy(
-        10000,
+        10000000000000000000000,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
